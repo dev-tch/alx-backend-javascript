ab6e1b5bfde9aa9da36109bb81c0074100f221b6
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-10 21:38:33 +0000

    fix promise three handler task2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,7 @@
 function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
 export default handleResponseFromAPI;
