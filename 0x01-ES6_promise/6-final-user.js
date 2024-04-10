1beedd53be79fadb36037cd6b22c8b937d5e8154
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-10 23:31:37 +0000

    fix task6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,7 +7,7 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
       data.map((res) => (
         {
           status: res.status,
-          value: res.status === 'fulfilled' ? res.value : res.reason,
+          value: res.status === 'fulfilled' ? res.value : String(res.reason),
         }
       ))
     ));
