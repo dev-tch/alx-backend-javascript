70c288699f24e78c92f63979f63d42f5a2210fcd
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-11 21:42:11 +0000

    clean code

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,18 +2,15 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    if (!(currency instanceof Currency) || typeof amount !== 'number') {
-      throw new Error('Invalid input types');
-    }
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  get cuurency() {
+  get currency() {
     return this._currency;
   }
 
@@ -24,7 +21,7 @@ export default class Pricing {
     this._amount = amount;
   }
 
-  set cuurency(currency) {
+  set currency(currency) {
     if (!(currency instanceof Currency)) {
       throw new Error('currency must be  instance of object Currency');
     }
