61373b24285ff3835c489ac11f5b5fd17378ed78
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-11 21:56:09 +0000

    abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new Error('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
