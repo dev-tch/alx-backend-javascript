642b942d30e4fa52ddada66eb3ec68c12ab91019
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-11 22:00:55 +0000

    task5 fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this.sqft = sqft;
   }
 
@@ -12,10 +15,5 @@ export default class Building {
       throw new Error('sqft must be a number');
     }
     this._sqft = sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
