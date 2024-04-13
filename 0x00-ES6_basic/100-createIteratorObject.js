2ef6aebd6965203f0e858480e0081ff49c59ba72
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-13 01:19:14 +0000

    return iterable of all employees

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+  for (const dep of Object.values(report.allEmployees)) {
+    employees.push(...dep);
+  }
+  return employees;
+}
