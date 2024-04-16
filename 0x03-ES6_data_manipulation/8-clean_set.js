e1b1e4ba6d5941e5a825b0c7d51a2adeb4360c0e
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-16 23:05:40 +0000

    manip string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+export default function cleanSet(setObj, startString) {
+  if (!startString || !(setObj instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  const index = startString.length;
+  const parts = [];
+  for (const item of setObj.values()) {
+    if (item.startsWith(startString)) {
+      const endString = item.substring(index);
+      if (endString && endString !== startString) {
+        parts.push(endString);
+      }
+    }
+  }
+  if (parts.length > 1) {
+    return parts.join('-');
+  }
+  return '';
+}
