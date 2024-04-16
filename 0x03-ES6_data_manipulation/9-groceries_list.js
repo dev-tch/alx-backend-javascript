9838ed994384b4302871c2115c238fe60b4f6c1c
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-16 23:50:57 +0000

    map object

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const map = new Map();
+  map.add('Apples', 10);
+  map.add('Tomatoes', 10);
+  map.add('Pasta', 1);
+  map.add('Rice', 1);
+  map.add('Banana', 5);
+  return map;
+}
