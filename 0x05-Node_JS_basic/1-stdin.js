47dd240c45cae046f3d18b50fac019e3216d3983
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-03 19:20:01 +0000

    use data and end events of process.stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (chunck) => {
+  process.stdout.write(`Your name is: ${chunck}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
