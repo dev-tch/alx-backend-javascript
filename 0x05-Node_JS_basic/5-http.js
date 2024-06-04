6402363f58d10f54fc8293334843f930b37d6125
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-04 19:58:22 +0000

    fix 5-http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,35 +1,108 @@
 const { createServer } = require('http');
 const { spawn } = require('child_process');
+const fs = require('fs');
+const { stdout } = require('process');
+
+class Department {
+  constructor(nameDep) {
+    this.nameDep = nameDep;
+    this.students = [];
+  }
+
+  append(nameSudent) {
+    if (this.students.includes(nameSudent)) {
+      return;
+    }
+    this.students.push(nameSudent);
+    Department.updateTotal();
+  }
+
+  static updateTotal() {
+    if (this.total === undefined) {
+      this.total = 0;
+    }
+    this.total += 1;
+  }
+
+  static getTotal() {
+    if (this.total === undefined) {
+      this.total = 0;
+    }
+    return this.total;
+  }
+
+  getStudents() {
+    return this.students;
+  }
+
+  getNameDep() {
+    return this.nameDep;
+  }
+
+  isNameDepEquals(nameDep) {
+    if (this.nameDep === nameDep) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const listDep = [];
+const output  = []
+function search(nameDep) {
+  for (const obj of listDep) {
+    if (obj.isNameDepEquals(nameDep)) {
+      return obj;
+    }
+  }
+  const obj = new Department(nameDep);
+  listDep.push(obj);
+  return obj;
+}
+const path = (process.argv.length === 3) ? process.argv[2] : '';
+const countStudents = (path) => new Promise((resolve, reject) => { 
+
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (!path || err) {
+      Promise.reject(new Error('Cannot load the database'));
+    }
+    const lines = data.split('\n');
+    for (const line of lines) {
+      if (!(line.trim() === '' || line.includes('firstname,lastname,age,field'))) {
+        const infoStd = line.split(',');
+        const objDep = search(infoStd[3]);
+        objDep.append(infoStd[0]);
+      }
+    }
+    output.push(`Number of students: ${Department.getTotal()}`);
+    for (const depObj of listDep) {
+      const students = depObj.getStudents();
+      output.push(`Number of students in ${depObj.getNameDep()}: ${students.length}. List: ${Array.prototype.join.call(students, ', ')}`);
+    }
+  });
+
+});
 
 const hostname = 'localhost';
 const port = 1245;
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  const path = (process.argv.length === 3) ? process.argv[2] : '';
-  const cmd = `(async () => {const countStudents = require('./3-read_file_async'); try { await countStudents('${path}');} catch(error) {console.log(error)}})()`;
-  let child = '';
-  let receivedData = '';
   switch (req.url) {
     case '/':
       res.end('Hello Holberton School!');
       break;
     case '/students':
-      child = spawn('node', ['-e', `${cmd}`]);
-      child.stdout.on('data', (data) => {
-        receivedData += data.toString();
-      });
-      child.stderr.on('data', (data) => {
-        res.statusCode = 500;
-        receivedData += data.toString();
-      });
-      child.on('close', () => {
-        res.end(`This is the list of our students\n${receivedData.trim()}`);
-      });
-      break;
-    default:
-      res.statusCode = 404;
-      res.end('Not Found');
+      countStudents(path)
+        .then((output) => {
+          output.splice(0, 0 , 'This is the list of our students')
+          res.end(output.join('\n'))
+        })
+        .catch( () => {
+          response.statusCode = 404;
+          response.end('Cannot load the database');
+        }
+        );  
       break;
   }
 });
