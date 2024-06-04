166622311d3e85a2fc742c5868425cfdda8fc3d8
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-04 21:36:27 +0000

    fx11

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -105,5 +105,6 @@ const app = createServer((req, res) => {
   }
 });
 app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 module.exports = app;
