95c193717fbbaf7052519a3fb1d5857af63b5276
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-07 17:09:55 +0000

    fix api test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,16 @@
 const { expect } = require('chai');
 const request = require('request');
-describe('APITEST', () => {
-  it('should return http response with status code 200 if not error aoccured', () => {
-    request('http://localhost:7865 /', function (error, response, body) {
-      if (!error) {
+describe('APITEST', function (){
+  it('should return http response with status code 200 if not error aoccured', function (done) {
+    request('http://localhost:7865/', function (error, response, body) {
+      if (error) {
+        done(error);
+      }
+      else {
         expect(response.statusCode).to.equal(200)
-        expect(response.text).to.equal('Welcome to the payment system')
+        expect(body).to.equal('Welcome to the payment system');
+        done();
       }
     });
   });
-
 });
