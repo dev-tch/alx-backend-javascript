d237a2c7ade0a8341c3dcd5c3396fd5c3df9e781
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-07 02:38:07 +0000

    task2 use chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,13 @@
+function calculateNumber(type='SUM', a, b=0) {
+  if (type === 'SUM') {
+    return (Math.round(a) +  Math.round(b))
+  }
+  if (type === 'SUBTRACT') {
+    return (Math.round(a) - Math.round(b))
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+}
+module.exports = calculateNumber;
+
