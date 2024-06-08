2706c004bafe4247ef38cb4a95254d92df70279a
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-08 02:14:02 +0000

    skip lines

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -3,15 +3,11 @@ import fs from 'fs';
 export function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     const students = {};
-
-    // Test access file path
     fs.access(filePath, fs.constants.R_OK, (err) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
-
-      // Read file contents
       fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) {
           reject(new Error('Cannot load the database'));
@@ -19,8 +15,6 @@ export function readDatabase(filePath) {
         }
 
         const lines = data.split('\n');
-
-        // Parse CSV header
         const header = lines[0].split(',');
         const fieldIndex = header.indexOf('field');
         const firstNameIndex = header.indexOf('firstname');
@@ -29,20 +23,20 @@ export function readDatabase(filePath) {
           reject(new Error('Invalid database format'));
           return;
         }
-
-        // Parse each row
         lines.slice(1).forEach((line) => {
-          const values = line.split(',');
+          if (line.trim() !== '') {
+            const values = line.split(',');
 
-          if (values.length === header.length) {
-            const field = values[fieldIndex];
-            const firstName = values[firstNameIndex];
+            if (values.length === header.length) {
+              const field = values[fieldIndex];
+              const firstName = values[firstNameIndex];
 
-            if (!students[field]) {
-              students[field] = [];
+              if (!students[field]) {
+                students[field] = [];
+              }
+
+              students[field].push(firstName);
             }
-
-            students[field].push(firstName);
           }
         });
 
